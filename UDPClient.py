8a6d6ac71c758d39e77bfc96d71a755c90e695b9
Enzo <96661578+Enzo1603@users.noreply.github.com>
2024-09-05 09:47:22 +0200

    add loop to UDPClient

--- UDPClient.py
+++ UDPClient.py
@@ -1,18 +1,19 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
 
-server_name = "192.168.178.15"
-server_port = 80
+server_name = "192.168.178.151"
+server_port = 12000
 
 # AF_INET is the address family for IPv4
 # SOCK_DGRAM is the socket type for UDP
 client_socket = socket(AF_INET, SOCK_DGRAM)
 
-message = input("Input lowercase sentence: ")
+while True:
+    message = input("Input lowercase sentence: ")
 
-client_socket.sendto(message.encode(), (server_name, server_port))
+    client_socket.sendto(message.encode(), (server_name, server_port))
 
-modified_message, server_address = client_socket.recvfrom(2048)
-print(modified_message.decode())
+    modified_message, server_address = client_socket.recvfrom(2048)
+    print(modified_message.decode())
 
 client_socket.close()
