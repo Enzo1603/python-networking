81888624e43651efa00d1a91d8150029c96d19a1
Enzo <96661578+Enzo1603@users.noreply.github.com>
2024-09-05 10:03:06 +0200

    Change port

--- TCPClient.py
+++ TCPClient.py
@@ -1,19 +1,18 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
 server_name = "192.168.178.151"
-server_port = 12000
+server_port = 12002
 
 # AF_INET is the address family for IPv4
 # SOCK_STREAM is the socket type for TCP
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((server_name, server_port))
 
-while True:
-    message = input("Input lowercase sentence: ")
+message = input("Input lowercase sentence: ")
 
-    client_socket.send(message.encode())
+client_socket.send(message.encode())
 
-    modified_message = client_socket.recv(1024)
-    print(modified_message.decode())
+modified_message = client_socket.recv(1024)
+print(modified_message.decode())
 
 client_socket.close()
