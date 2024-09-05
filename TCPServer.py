44b9dbbbc42feda8828511c291ca145c5e3b747a
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2024-09-05 10:02:41 +0200

    change port

--- TCPServer.py
+++ TCPServer.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-server_port = 12000
+server_port = 12002
 
 # AF_INET is the address family for IPv4
 # SOCK_STREAM is the socket type for TCP
@@ -10,12 +10,14 @@ server_socket.listen(1)
 print("The server is ready to receive.")
 
 while True:
-    connection_socket, addr = server_socket.accept()
+    connection_socket, client_address = server_socket.accept()
     message = connection_socket.recv(1024).decode()
+    print(f"Received message: '{message}' from {client_address}")
 
     modified_message = message.upper()
 
     connection_socket.send(modified_message.encode())
+    print("Sent modified message back to client.")
     connection_socket.close()
 
 server_socket.close()
