3f99f70147f615f31d483b76c020fda59d585194
Enzo <96661578+Enzo1603@users.noreply.github.com>
2024-09-05 09:42:32 +0200

    Add UDPServer.py

---
+++ UDPServer.py
@@ -0,0 +1,16 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind(("", server_port))
+print("The server is ready to receive.")
+
+while True:
+    message, client_address = server_socket.recvfrom(2048)
+    print(f"Received message: {message.decode()} from {client_address}")
+
+    modified_message = message.decode().upper()
+    server_socket.sendto(modified_message.encode(), client_address)
+    print("Sent modified message back to client.")
+
+server_socket.close()
